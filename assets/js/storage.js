de8f129fad8e98ea1d6bbbd2f74aed3330a0233d
castefeudal <castefeudal@gmail.com>
2026-08-25 13:30:55 +0300

    fix: bind Pro controls directly after render

--- assets/js/storage.js
+++ assets/js/storage.js
@@ -1,4 +1,4 @@
-import { normalizeImport } from './validators.js?v=5.2.0-r3';
+import { normalizeImport } from './validators.js?v=5.2.0-r4';
 
 const KEY = 'markovlab-state-v3';
 const LEGACY_KEYS = ['markovlab-state-v2','markovlab-state-v1'];
