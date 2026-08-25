25aa9613340b455fb7ad91faa4d26a62b201e2a8
castefeudal <castefeudal@gmail.com>
2026-08-25 13:30:51 +0300

    fix: bind Pro controls directly after render

--- assets/js/state.js
+++ assets/js/state.js
@@ -1,1 +1,1 @@
-export { DEFAULT_STATE, migrateState, loadState, saveState, exportState, importState, clearState, addHistory, addSnapshot, touchRecent } from './storage.js?v=5.2.0-r3';
+export { DEFAULT_STATE, migrateState, loadState, saveState, exportState, importState, clearState, addHistory, addSnapshot, touchRecent } from './storage.js?v=5.2.0-r4';
